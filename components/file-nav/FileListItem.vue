6bef4573451df3b145e76a96ece9e3300729dacc
Lenson <53988015+Lebarnon@users.noreply.github.com>
2023-08-17 23:23:41 +0800

    Update FileListItem.vue

--- components/file-nav/FileListItem.vue
+++ components/file-nav/FileListItem.vue
@@ -1,11 +1,14 @@
 <template>
-  <q-item clickable v-ripple>
+  <q-item clickable v-ripple
+    @click="onAppDocumentClicked()"
+    :class="isSelected ? 'bg-grey' : ''"
+  >
     <q-item-section avatar top>
-      <q-avatar :icon="icon" color="grey" text-color="white" />
+      <q-avatar :icon="appDocument.icon" color="grey" text-color="white" />
     </q-item-section>
 
     <q-item-section>
-      <q-item-label lines="1">{{ name }}</q-item-label>
+      <q-item-label lines="1">{{ appDocument.name }}</q-item-label>
       <q-item-label caption>March 2nd, 2019</q-item-label>
     </q-item-section>
 
@@ -16,6 +19,14 @@
 </template>
 
 <script setup lang="ts">
-const props = defineProps(['name','icon', 'selected'])
+import {AppDocument} from '@/types/types'
+const props = defineProps<{
+  appDocument: AppDocument, 
+  isSelected: boolean, 
+}>()
+const emits = defineEmits(['onAppDocumentClicked'])
+function onAppDocumentClicked(){
+  emits("onAppDocumentClicked")
+}
 
 </script>
