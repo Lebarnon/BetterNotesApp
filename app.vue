80957888c04025878e0e67ceaa3c7206fc4a9cf7
Lenson <53988015+Lebarnon@users.noreply.github.com>
2023-08-17 23:23:46 +0800

    Update app.vue

--- app.vue
+++ app.vue
@@ -8,5 +8,11 @@
 </template>
 
 <script setup lang="ts">
+import { useCollectionsStore } from '@/store/collections';
 
+onBeforeUnmount(() => {
+  for(const unsub of useCollectionsStore().unsubscribes){
+    unsub()
+  }
+})
 </script>
