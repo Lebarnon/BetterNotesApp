274061a06aa52b82ca58a1156a10665c10476e11
Lenson <53988015+Lebarnon@users.noreply.github.com>
2023-08-17 23:23:37 +0800

    Now subscribes to documents

--- store/documents.ts
+++ store/documents.ts
@@ -1,6 +1,6 @@
 import { defineStore } from 'pinia'
 import { AppCollection, AppDocument } from '@/types/types'
-import { addDoc, arrayUnion, collection, doc, getDocs, serverTimestamp, updateDoc } from 'firebase/firestore'
+import { addDoc, arrayUnion, collection, deleteDoc, doc, getDocs, onSnapshot, serverTimestamp, updateDoc } from 'firebase/firestore'
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import collectionfactory from '@/utils/factory/collectionfactory'
 import { useUserStore } from './user'
@@ -8,7 +8,9 @@ import { useCollectionsStore } from './collections';
 import documentfactory from '@/utils/factory/documentfactory';
 export const useDocumentsStore = defineStore('documents', {
   state: () => ({
-    documents: {} as {[collectionId: string ]: Array<AppDocument>;},
+    documents: [] as Array<AppDocument>,
+    selectedDocument: null as AppDocument | null,
+    prevSubscription: null as any
   }),
   getters: {
     getUser(state) {
@@ -20,36 +22,56 @@ export const useDocumentsStore = defineStore('documents', {
     },
     getCollection(state){
       const col: AppCollection | null = useCollectionsStore().getSelectedCollection
-      if(col == null){
-        throw new Error("No Collection Selected")
-      }
       return col
     },
     getDocuments(state){
-      const curCollection = useCollectionsStore().getSelectedCollection
-      if(curCollection){
-        return curCollection.id in state.documents ? state.documents[curCollection.id] : null
-      }
-      return null
+      return state.documents
+    },
+    getSelectedDocument(state){
+      return state.selectedDocument
+    },
+    isDocumentSelect(state){
+      return (appDocument: AppDocument) => appDocument.id == this.selectedDocument?.id
     }
   },
   actions: {
     // subscribe to collections
-    async fetchDocuments(){
+    async setDocuments(){
       try{
-        const {$firestore, $storage} = useNuxtApp()
-        const curCollection = this.getCollection
-        const user = this.getUser
-        if(curCollection.id in this.documents){
-          return
+        const {$firestore} = useNuxtApp()
+        // unsubscribe to previous if needed
+        if(this.prevSubscription){
+          this.prevSubscription()
         }
-        const querySnapshot = await getDocs(collection($firestore, "users", user.uid, "collections", curCollection.id, "documents"))
-        querySnapshot.forEach((doc) => {
-          // doc.data() is never undefined for query doc snapshots
-          this.addDocumentToState(curCollection.id,documentfactory.fromFirestore(doc.data()));
+        // reset documents
+        this.$reset()
+
+        const curCollection = this.getCollection
+        if(curCollection == null){
+          throw new Error("No Collection Selected")
+        }
+        const user = this.getUser
+        const collectionRef = collection($firestore, "users", user.uid, "collections", curCollection.id, "documents")
+
+
+        const unsubscribe = onSnapshot(collectionRef, (snapshot) => {
+          snapshot.docChanges().forEach((change) => {
+            if (change.type === "added") {
+              this.addDocumentToState(curCollection.id, change.doc.id, documentfactory.fromFirestore(change.doc.data()));
+            }
+            if (change.type === "modified") {
+              console.log("modified called")
+              this.updateDocumentInState(curCollection.id, documentfactory.fromFirestore(change.doc.data()));
+            }
+            if (change.type === "removed") {
+              this.deleteDocumentInState(curCollection.id, documentfactory.fromFirestore(change.doc.data()));
+            }
+          });
         });
+
+        return unsubscribe
+
       }catch(error){
-        console.error(error)
         Notify.create({message: ""+error, color:"negative"})
       }
     },
@@ -57,8 +79,10 @@ export const useDocumentsStore = defineStore('documents', {
         try{
             const {$firestore, $storage} = useNuxtApp()
             const curCollection = this.getCollection
+            if(curCollection == null){
+              throw new Error("No Collection Selected")
+            }
             const user = this.getUser
-
 
             // Create the file metadata
             /** @type {any} */
@@ -66,8 +90,8 @@ export const useDocumentsStore = defineStore('documents', {
               contentType: file.type
             };
             // Upload file and metadata to the object 'images/mountains.jpg'
-            const storageRef = ref($storage, `${user.uid}/${curCollection.id}/${file.name}`);
-            
+            const docRef = await addDoc(collection($firestore, "users", user.uid, "collections", curCollection.id, "documents"), documentfactory.defaultDocument(file));
+            const storageRef = ref($storage, `${user.uid}/documents/${curCollection.id}/${docRef.id}`);
             
             doc($firestore, "users", user.uid);
             const uploadTask = uploadBytesResumable(storageRef, file, metadata);
@@ -90,6 +114,7 @@ export const useDocumentsStore = defineStore('documents', {
               (error) => {
                 // A full list of error codes is available at
                 // https://firebase.google.com/docs/storage/web/handle-errors
+                deleteDoc(docRef)
                 switch (error.code) {
                   case 'storage/unauthorized':
                     // User doesn't have permission to access the object
@@ -108,24 +133,52 @@ export const useDocumentsStore = defineStore('documents', {
               async () => {
                 // Upload completed successfully, now we can get the download URL
                 const downloadURL = await getDownloadURL(uploadTask.snapshot.ref)
-                const docRef = await addDoc(collection($firestore, "users", user.uid, "collections", curCollection.id, "documents"), documentfactory.defaultDocument(file));
                 updateDoc(docRef, {
                   id: docRef.id,
                   downloadURL: downloadURL
                 });
-                const newAppDoc = documentfactory.defaultDocument(file)
-                newAppDoc.id = docRef.id
-                newAppDoc.downloadURL = downloadURL
-                this.addDocumentToState(curCollection.id, newAppDoc)
               }
             );
         }catch(error){
-            console.error("Failed to upload file")
+            console.error("Failed to upload file: " + error)
             Notify.create({message:"Failed to upload file", color:"negative"})
         }
     },
-    addDocumentToState(collectionId: string, appDocument: AppDocument){
-      (this.documents[collectionId] ??= []).push(appDocument);
+    async deleteDoc(appDocument: AppDocument){
+      try{
+        const {$firestore} = useNuxtApp()
+        const curCollection = this.getCollection
+          if(curCollection == null){
+            throw new Error("No Collection Selected")
+          }
+          const user = this.getUser
+        deleteDoc(doc($firestore, "users", user.uid, "collections", curCollection.id, "documents", appDocument.id))
+        if(this.selectedDocument?.id == appDocument.id){
+          this.selectedDocument=null
+        }
+      }catch(error){
+        console.error(error)
+        Notify.create({message: "Failed to delete file:"+error, color:"negative"})
+      }
+    },
+    setSelectedDocument(appDocument: AppDocument){
+      this.selectedDocument = appDocument
+    },
+    addDocumentToState(collectionId: string, appDocumentId: string, appDocument: AppDocument){
+      appDocument.id = appDocumentId;
+      this.documents.push(appDocument);
+    },
+    updateDocumentInState(collectionId: string, appDocument:AppDocument){
+      const index = this.documents.findIndex((el) => el.id == appDocument.id)
+      if(index != -1){
+        this.documents.splice(index, 1, documentfactory.fromFirestore(appDocument))
+      }
+    },
+    deleteDocumentInState(collectionId: string, appDocument: AppDocument){
+      const index = this.documents.findIndex((el) => el.id == appDocument.id)
+      if(index != -1){
+        this.documents.splice(index,1)
+      }
     }
   }
 })
